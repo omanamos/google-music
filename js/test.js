f53066ba907025f16f4b9e4c906001e3fc9df183
Ryan Oman <omanamos@gmail.com>
2012-02-02 12:09:26 -0600

    got basic functionality of removing dups working

--- js/test.js
+++ js/test.js
@@ -1,12 +1,16 @@
+var MAX_SONGS = 500;
+
 // this supports trigger native events such as 'onchange'
 // whereas prototype.js Event.fire only supports custom events
-function trigger(element, eventName) {
+function trigger(element, eventName, ctrl) {
     // safari, webkit, gecko
-    if (document.createEvent)
-    {
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent(eventName, true, true);
- 
+    if (document.createEvent) {
+        var evt = document.createEvent('HTMLEvents');
+        evt.initEvent(eventName, true, true);
+        if (ctrl) {
+            evt.ctrlKey = ctrl;
+        }
+
         return element.dispatchEvent(evt);
     }
  
@@ -16,16 +20,29 @@ function trigger(element, eventName) {
     }
 }
 
+function getMenuContainer() {
+    var menus = document.getElementsByClassName("goog-menu goog-menu-vertical");
+    for (var i = 0; i < menus.length; i++) {
+        if (menus[i].children.length == 10) {
+            return menus[i];
+        }
+    }
+}
+
+function getRemoveButton() {
+    return getMenuContainer().getElementsByClassName('goog-menuitem')[6];
+}
+
 // Removes given song from playlist
 // Give it the row element that the song is in
 // Example: select a song and then run "removeSong($$("tr.selectedSong")[0])" in
 // the console (enter in functions first though).
 function removeSong(rowElement) {
     var a = rowElement.getElementsByClassName('fade-out-with-menu')[0];
+    var c = getRemoveButton();
     trigger(a, 'mouseover');
     var b = a.children[0];
     trigger(b, 'click');
-    var c = document.getElementById(":2f")
     trigger(c, 'mousedown');
     trigger(c, 'mouseup');
 }
@@ -39,6 +56,27 @@ function buildSong(row) {
         count: Number(children[4].innerHTML.trim()),
         element: row
     };
+}
+
+function remove(duplicateSongs, init) {
+    var element;
+    for (var n = init; n < Math.min(init + MAX_SONGS, duplicateSongs.length); n++) {
+        if (duplicateSongs[n].element.className.indexOf("selectedSong") == -1) {
+            element = duplicateSongs[n].element;
+            trigger(element, 'click', true);
+        }
+    }
+    if (element) {
+        removeSong(element);
+    }
+    return Math.min(init + MAX_SONGS, duplicateSongs.length) != duplicateSongs.length
+}
+
+function waitForAjax() {
+    var hasPending = true;
+    while (hasPending) {
+        setTimeout("hasPending = ", 1000);
+    }
 }
 
 function dedup() {
@@ -69,9 +107,10 @@ function dedup() {
         }
     }
 
-    for (n = 0; n < duplicateSongs.length; n++) {
-        duplicateSongs[n].element.className = "selectedSong " + duplicateSongs[n].element.className;
+    var init = 0;
+    var keepGoing = remove(duplicateSongs, init);
+    while (keepGoing) {
+        init += MAX_SONGS;
+        setTimeout("keepGoing = remove(duplicateSongs, init)", 200);
     }
 }
-
-
